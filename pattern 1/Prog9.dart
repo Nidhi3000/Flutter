f9eb979a6a7763cae2cee6b444bb91f157731937
Nidhi Kalanke <94119949+Nidhi3000@users.noreply.github.com>
2024-08-28 01:49:49 +0530

    Update Prog9.dart

--- pattern 1/Prog9.dart
+++ pattern 1/Prog9.dart
@@ -1,17 +1,15 @@
 import "dart:io";
-void main(){
-  print("Enter no of rows");
-  int rows=int.parse(stdin.readLineSync()!);
-  int temp =1;
-  for(int i=0;i<rows;i++){
-    for(int j=0; j<rows;j++){
-      stdout.write("$temp ");
-      if(j!=rows-1){
-        temp++;
-      }
 
+void main() {
+  int rows = int.parse(stdin.readLineSync()!);
+  int counter = 1;
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < rows; j++) {
+      stdout.write("$counter ");
+      counter++;
     }
     print("");
-  }
-    
+    counter = counter - 1;
+  }
 }
