5e6627b6409eec21e0f35cd334ee346badfb78af
Nidhi Kalanke <94119949+Nidhi3000@users.noreply.github.com>
2024-08-28 01:48:11 +0530

    Update Prog6.dart

--- pattern 1/Prog6.dart
+++ pattern 1/Prog6.dart
@@ -3,7 +3,7 @@ import "dart:io";
 void main() {
   print("Enter no of rows");
   int rows = int.parse(stdin.readLineSync()!);
-  for (int i = 14; i <= 14+rows; i++) {
+  for (int i = 14; i < 14+rows; i++) {
     for (int j = 1; j <= rows; j++) {
       stdout.write("$i ");
     }
